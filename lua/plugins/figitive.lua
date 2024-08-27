74cdaa3f2f05044faa23c9675fca6d5d9844580c
Alaricode <antonlarichev@gmail.com>
2024-08-27 15:32:21 +0300

    Figitive

---
+++ lua/plugins/figitive.lua
@@ -0,0 +1,11 @@
+return {
+	{ "tpope/vim-fugitive" },
+	{
+		"rbong/vim-flog",
+		lazy = true,
+		cmd = { "Flog", "Flogsplit", "Floggit" },
+		dependencies = {
+			"tpope/vim-fugitive",
+		},
+	},
+}
